8408ef6028f61876cccc43887b9fb30e82c5380f
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-09-26 14:09:52 +0300

    Updated integration test

--- integrationTest.sh
+++ integrationTest.sh
@@ -6,6 +6,9 @@ mkdir outputDir
 # Run the Scala app with sbt
 sbt "run plot --token pk.eyJ1IjoiYXJuYXNiciIsImEiOiJjbG00dXY1MDAybGJrM2RwNnE2dmo1NW01In0.XC_idJ6KnMWc1N-MX-Ry7A --swap --input integrationTestData/integrationTestInput.json"
 
+# Wait for the image to be downloaded
+sleep 10
+
 # Check if the newly downloaded image matches the expected image
 if cmp -s "outputDir/output.png" "integrationTestData/integrationTestOutput.png"; then
    echo "App works as intended"

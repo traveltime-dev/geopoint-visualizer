6e013e553eeab71ea38f20a967b443c15a31d9ab
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-10-02 15:43:47 +0300

    Improved intergrationTest.sh

--- integrationTest.sh
+++ integrationTest.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check if any arguments are passed
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./integrationTest.sh <MAPBOX_API_TOKEN>"
+  echo "You must provide the Mapbox API token as an argument."
+  exit 1
+fi
+
+# Check if outputDir/output.png exists and delete it
+if test -f "outputDir/output.png"; then
+  echo "Deleting existing outputDir/output.png."
+  rm -f "outputDir/output.png"
+fi
+
 token=$1
 
 # Run the Scala app with sbt
@@ -7,9 +20,9 @@ sbt "run plot --token $token --swap --input integrationTestData/integrationTestI
 
 # Check if the output.png file exists
 if test -f "outputDir/output.png"; then
-   echo "outputDir/output.png exists."
-   exit 0
+  echo "outputDir/output.png exists."
+  exit 0
 else
-   echo "outputDir/output.png does not exist."
-   exit 1
+  echo "outputDir/output.png does not exist."
+  exit 1
 fi
